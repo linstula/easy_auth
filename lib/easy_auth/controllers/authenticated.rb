5acae3df805693842af2ed12a8b447d23ec18a58
Lin Reid <linreid@gmail.com>
2013-11-01 15:17:21 -0400

    pickup from method missing for stash_resource

--- lib/easy_auth/controllers/authenticated.rb
+++ lib/easy_auth/controllers/authenticated.rb
@@ -7,13 +7,20 @@ module EasyAuth::Controllers::Authenticated
 
   private
 
-  def attempt_to_authenticate
+  def attempt_to_authenticate(resource=nil)
     if account_not_signed_in?
       session[:requested_path] = request.path
+      session[resource] = params[resource] if resource
       respond_to do |format|
         format.html { redirect_to main_app.sign_in_url }
         format.json { render :json => {}, :status => 401 }
       end
     end
+  end
+
+  def method_missing(resource, *args)
+    resource_str = resource.to_s
+    return resource_str[6..-1] if resource_str[/\Astash_/]
+    super
   end
 end
